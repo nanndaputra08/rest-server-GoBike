bd4a0f42aa43024462a53d4ca26a918f1c6444aa
Nanda Putra Utama <nanndaputraa08@gmail.com>
2020-06-30 15:23:09 +0700

    Created End Point Jenis Sepeda

--- application/controllers/api/Jenis_Sepeda.php
+++ application/controllers/api/Jenis_Sepeda.php
@@ -30,4 +30,55 @@ class Jenis_Sepeda extends REST_Controller{
             ], REST_Controller::HTTP_OK);
         }
     }
+
+    public function index_post(){
+
+        $nama_jenis_sepeda = $this->post('nama_jenis_sepeda');
+
+        $data = [
+            'nama_jenis_sepeda' => $nama_jenis_sepeda
+        ];
+
+        $result = $this->Model_Jenis_Sepeda->createJenisSepeda($data);
+    
+        if ($result > 0){
+
+            $this->response([
+                'status' => true,
+                'message' => 'Berhasil menambahkan data Jenis Sepeda',
+            ], REST_Controller::HTTP_CREATED);
+        }
+    }
+
+    public function index_delete(){
+
+        $id = $this->delete('id');
+
+        if ($id === null){
+
+            $this->response([
+                'status' => false,
+                'message' => 'Id tidak boleh kosong.'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+
+            $result = $this->Model_Jenis_Sepeda->deleteJenisSepeda($id);
+
+            if ($result > 0){
+
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'Data Jenis Sepeda berhasil dihapus.'
+                ], REST_Controller::HTTP_OK);
+            } else {
+
+                $this->response([
+                    'status' => false,
+                    'id' => $id,
+                    'message' => 'Id tidak ditemukan.'
+                ], REST_Controller::HTTP_NOT_FOUND);
+            }
+        }
+    }
 }
--- application/models/Model_Jenis_Sepeda.php
+++ application/models/Model_Jenis_Sepeda.php
@@ -5,8 +5,22 @@ class Model_Jenis_Sepeda extends CI_Model{
 
     public function getJenisSepeda(){
 
-        $result = $this->db->query("SELECT * FROM tbl_jenis_speda")->result_array();
+        $result = $this->db->query("SELECT * FROM tbl_jenis_sepeda")->result_array();
 
         return $result;
+    }
+
+    public function createJenisSepeda($data){
+
+        $result = $this->db->insert('tbl_jenis_sepeda', $data);
+
+        return $this->db->affected_rows();
+    }
+
+    public function deleteJenisSepeda($id){
+
+        $result = $this->db->query("DELETE FROM tbl_jenis_sepeda WHERE tbl_jenis_sepeda.id='$id'");
+
+        return $this->db->affected_rows();
     }
 }
